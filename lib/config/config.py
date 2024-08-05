2fb999c57d1545e5a92310361f2267682989636f
yunzhiy <yanyz@zju.edu.cn>
2024-08-05 11:05:57 +0800

    Update configuration

--- lib/config/config.py
+++ lib/config/config.py
@@ -12,12 +12,12 @@ cfg.loaded_iter = -1
 cfg.ip = '127.0.0.1'
 cfg.port = 6009
 cfg.data_device = 'cuda'
-cfg.mode = 'train'
-cfg.task = 'hello'
-cfg.exp_name = 'test'
-cfg.gpus = [0]
+cfg.mode = 'train' 
+cfg.task = 'hello' # task folder name
+cfg.exp_name = 'test' # experiment folder name
+cfg.gpus = [0] # list of gpus to use 
 cfg.debug = False
-cfg.resume = True
+cfg.resume = True # If set to True, resume training from the last checkpoint.
 
 cfg.source_path = ''
 cfg.model_path = ''
@@ -26,8 +26,8 @@ cfg.resolution = -1
 cfg.resolution_scales = [1]
 
 cfg.eval = CN()
-cfg.eval.skip_train = False
-cfg.eval.skip_test = False
+cfg.eval.skip_train = False 
+cfg.eval.skip_test = False 
 cfg.eval.eval_train = False
 cfg.eval.eval_test = True
 cfg.eval.quiet = False
@@ -44,7 +44,8 @@ cfg.train.start_checkpoint = None
 cfg.train.importance_sampling = False
 
 cfg.optim = CN()
-cfg.optim.position_lr_init = 0.00016
+# learning rate
+cfg.optim.position_lr_init = 0.00016 # position_lr_init_{bkgd, obj ...}, similar to the following
 cfg.optim.position_lr_final = 0.0000016
 cfg.optim.position_lr_delay_mult = 0.01
 cfg.optim.position_lr_max_steps = 30000
@@ -52,14 +53,19 @@ cfg.optim.feature_lr = 0.0025
 cfg.optim.opacity_lr = 0.05
 cfg.optim.scaling_lr = 0.005
 cfg.optim.rotation_lr = 0.001
+# densification and pruning
 cfg.optim.percent_dense = 0.01 
 cfg.optim.densification_interval = 100
 cfg.optim.opacity_reset_interval = 3000
 cfg.optim.densify_from_iter = 500
 cfg.optim.densify_until_iter = 15000
-cfg.optim.densify_grad_threshold = 0.0002
-
-
+cfg.optim.densify_grad_threshold = 0.0002 # densify_grad_threshold_{bkgd, obj ...}
+cfg.optim.densify_grad_abs_bkgd = False # densification strategy from AbsGS
+cfg.optim.densify_grad_abs_obj = False 
+cfg.optim.max_screen_size = 20
+cfg.optim.min_opacity = 0.005
+cfg.optim.percent_big_ws = 0.1
+# loss weight
 cfg.optim.lambda_l1 = 1.
 cfg.optim.lambda_dssim = 0.2
 cfg.optim.lambda_sky = 0.
@@ -74,48 +80,56 @@ cfg.optim.lambda_pose_correction = 0.
 cfg.optim.lambda_scale_flatten = 0.
 cfg.optim.lambda_opacity_sparse = 0.
 
-cfg.optim.max_screen_size = 20
-cfg.optim.min_opacity = 0.005
-cfg.optim.percent_big_ws = 0.1
 
 cfg.model = CN()
 cfg.model.gaussian = CN()
 cfg.model.gaussian.sh_degree = 3
+cfg.model.gaussian.fourier_dim = 1 # fourier spherical harmonics dimension
+cfg.model.gaussian.fourier_scale = 1.
+cfg.model.gaussian.flip_prob = 0. # symmetry prior for rigid objects, flip gaussians with this probability during training
 cfg.model.gaussian.semantic_mode = 'logits'
+
 cfg.model.nsg = CN()
-cfg.model.nsg.include_bkgd = True
-cfg.model.nsg.include_obj = True
-cfg.model.nsg.include_sky = False
-cfg.model.nsg.opt_track = True
+cfg.model.nsg.include_bkgd = True # include background
+cfg.model.nsg.include_obj = True # include object
+cfg.model.nsg.include_sky = False # include sky cubemap
+cfg.model.nsg.opt_track = True # tracklets optimization
 cfg.model.sky = CN()
 cfg.model.sky.resolution = 1024
 cfg.model.sky.white_background = True
 
-cfg.model.use_color_correction = False
-cfg.model.color_correction = CN()
-cfg.model.color_correction.mode = 'image' # [image, sensor]
-cfg.model.color_correction.use_mlp = False
 
-cfg.model.use_pose_correction = False
+#### Note: We have not fully tested this.
+cfg.model.use_color_correction = False # If set to True, learn transformation matrixs for appearance embedding
+cfg.model.color_correction = CN() 
+cfg.model.color_correction.mode = 'image' # If set to 'image', learn separate embedding for each image. If set to 'sensor', learn a single embedding for all images captured by one camera senosor. 
+cfg.model.color_correction.use_mlp = False # If set to True, regress embedding from extrinsic by a mlp. Otherwise, define the embedding explicitly.
+cfg.model.color_correction.use_sky = False # If set to True, using spparate embedding for background and sky
+# Alternative choice from GOF: https://github.com/autonomousvision/gaussian-opacity-fields/blob/main/scene/appearance_network.py
+
+cfg.model.use_pose_correction = False # If set to True, use pose correction for camera poses. 
 cfg.model.pose_correction = CN()
-cfg.model.pose_correction.mode = 'image' # [image, frame, all]
-
+cfg.model.pose_correction.mode = 'image' # If set to 'image', learn separate correction matrix for each image. If set to 'frame', learn a single correction matrix for all images corresponding to the same frame timestamp. 
+####
 
 cfg.data = CN()
-cfg.data.white_background = False
+cfg.data.white_background = False # If set to True, use white background. Should be False when using sky cubemap.
+cfg.data.use_colmap_pose = False # If set to True, use colmap to recalibrate camera poses as input (rigid bundle adjustment now).
+cfg.data.filter_colmap = False # If set to True, filter out SfM points by camera poses.
+cfg.data.box_scale = 1.0 # Scale the bounding box by this factor.
+cfg.data.split_test = -1 
 cfg.data.shuffle = True
-cfg.data.split_test = -1
 cfg.data.eval = True
 cfg.data.type = 'Colmap'
 cfg.data.images = 'images'
-cfg.data.use_colmap = True
-cfg.data.use_colmap_pose = False
-cfg.data.filter_colmap = False
-cfg.data.use_lidar = True
 cfg.data.use_semantic = False
 cfg.data.use_mono_depth = False
 cfg.data.use_mono_normal = False
-cfg.data.box_scale = 1.0
+cfg.data.use_colmap = True
+# data.load_pcd_from: Load the initialization point cloud from a previous experiment without generation.
+# data.extent: radius of the scene, we recommend 10 - 20 meters.
+# data.sphere_scale: Scale the sphere radius by this factor.
+# data.regenerate_pcd: Regenerate the initialization point cloud.
 
 cfg.render = CN()
 cfg.render.convert_SHs_python = False
@@ -126,13 +140,10 @@ cfg.render.fps = 24
 cfg.render.render_normal = False
 cfg.render.save_video = True
 cfg.render.save_image = True
-cfg.render.save_box = False
-cfg.render.edit = False
-
 cfg.render.coord = 'world' # ['world', 'vehicle']
-
+cfg.render.concat_cameras = []
 cfg.viewer = CN()
-cfg.viewer.frame_id = 0
+cfg.viewer.frame_id = 0 # Select the frame_id (start from 0) to save for viewer
 
 
 parser = argparse.ArgumentParser()
